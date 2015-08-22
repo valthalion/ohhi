1066c5b8338c25fdbc105301acf0d79712633c77
Diego Diaz Fidalgo <valthalion@gmail.com>
2015-08-21 22:25:31 -0700

    some cleaning up, enhanced documentation; added a modified version of puzzle3 that should result in infeasibility

--- cp_solver.py
+++ cp_solver.py
@@ -10,7 +10,8 @@ def propagate_constraints(problem, constraints):
     Each constraint in constraints should update the values in the problem
     and return True if changes were made, False otherwise.
 
-    Apply each constraint, and repeat until no changes are made in one cycle.
+    Apply each constraint, and repeat until no changes are made in a full
+    cycle.
     """
     changes = True
     while changes:
@@ -22,7 +23,7 @@ def get_undecided_variable(problem):
     Return one variable that is still unset in the problem
     """
     for variable, domain in problem['variables'].iteritems():
-        if len(domain) > 1:
+        if len(domain) > 1:  # Undecided if more than 1 value possible
             return variable
 
 
@@ -45,12 +46,33 @@ def solve(problem, constraints, evaluate_state):
     """
     Solve a constraint programming problem
 
-    TODO: Document inputs/output
+    Inputs:
+    - problem: a dictionary containing the information for the problem to be
+        solved. It may contain additional fields for use in the constraint
+        propagation functions, or the state evaluation function (or for any
+        other reason), but it must contain at least the following fields:
+        * 'variables': a dictionary with the variables to be solved as keys;
+            the corresponding value is the domain of the variable, an iterable
+            with all currently feasible values for the variable. This function
+            will assume that a variable is set (decided) if its domain is of
+            length 1
+        * 'state': the state of the problem; typically at call time it will be
+            'unsolved'. It will be updated as appropriate to 'solved' or
+            'infeasible'.
+    - constraints: an iterable containing the constraint propagation functions
+        available to the CP solver. Each one must accept a problem dictionary
+        and apply update the domains of all variables where the constraint
+        allows inference; this may mean determining one value, or reducing the
+        current domain by emoving some of its elements
+    - evaluate_state: a function that takes a problem as input and determines
+        whether the problem is still unsolved, in an incoherent state
+        (infeasible) or solved, and updates its 'state' field accordingly.
 
-    Returns the problem at the last stage in the solution
-    process. The problem 'state' field will be set to
-    'solved' or 'infeasible' as adequate.
+    Returns:
+    - the problem at the last stage in the solution process. The problem
+        'state' field will be set to 'solved' or 'infeasible' as adequate.
     """
+
     # Recursively explore the solution space:
     # - Fill in inferrable variables (constraint propagation)
     # - If this fully solves the problem, or identifies
@@ -79,5 +101,6 @@ def solve(problem, constraints, evaluate_state):
         candidate_sol = solve(new_problem, constraints, evaluate_state)
         if candidate_sol['state'] == 'solved':
             return candidate_sol
+
     # if this point is reached, the problem must be infeasible
     return candidate_sol
--- ohhi.py
+++ ohhi.py
@@ -53,7 +53,7 @@ def print_problem(problem):
     print
 
 
-def solved(problem):
+def determined(problem):
     """
     Check if the problem has all cells set to a colour. Returns
     True/False accordingly
@@ -66,12 +66,11 @@ def solved(problem):
 
 def eliminate_contiguous(problem):
     """
-    Update the problem by setting the cells that can be
-    inferred due to the constraint of no more than two
-    contiguous cells of the same color.
+    Update the problem by setting the cells that can be inferred due to the
+    constraint of no more than two contiguous cells of the same color.
 
-    The cells dictionary in problem is modified. The return
-    value is True if changes were made, False otherwise.
+    The cells dictionary in problem is modified. The return value is True if
+    changes were made, False otherwise.
     """
     changes = False
     size = problem['size']
@@ -81,55 +80,55 @@ def eliminate_contiguous(problem):
     changes |= eliminate_contiguous_line(cells,
                                          rows=range(size - 2),
                                          cols=range(size),
-                                         mask=[(1, 0), (2, 0)])
+                                         mask=((1, 0), (2, 0)))
     # Apply column-wise, down
     changes |= eliminate_contiguous_line(cells,
                                          rows=range(2, size),
                                          cols=range(size),
-                                         mask=[(-1, 0), (-2, 0)])
+                                         mask=((-1, 0), (-2, 0)))
     # Apply column-wise, gap
     changes |= eliminate_contiguous_line(cells,
                                          rows=range(1, size - 1),
                                          cols=range(size),
-                                         mask=[(1, 0), (-1, 0)])
+                                         mask=((1, 0), (-1, 0)))
     # Apply row-wise, left
     changes |= eliminate_contiguous_line(cells,
                                          rows=range(size),
                                          cols=range(size - 2),
-                                         mask=[(0, 1), (0, 2)])
+                                         mask=((0, 1), (0, 2)))
     # Apply row-wise, right
     changes |= eliminate_contiguous_line(cells,
                                          rows=range(size),
                                          cols=range(2, size),
-                                         mask=[(0, -1), (0, -2)])
+                                         mask=((0, -1), (0, -2)))
     # Apply row-wise, gap
     changes |= eliminate_contiguous_line(cells,
                                          rows=range(size),
                                          cols=range(1, size - 1),
-                                         mask=[(0, 1), (0, -1)])
+                                         mask=((0, 1), (0, -1)))
     return changes
 
 
 def eliminate_contiguous_line(cells, rows, cols, mask):
     """
-    Apply the no more than two contiguous cells of the same colour
+    Apply the constraint that there may be no more than two contiguous cells of
+    the same colour.
 
-    Check each (row, column) cell as specified by the rows and
-    columns lists, and using the mask ((r0, c0), (r1, c1)) to
-    define the direction: for cell (r, c), cells (r+r0, c+c0) and
-    (r+r1, c+c1) will be considered contiguous; if both are set to
-    the same colour, and (r, c) is unset, (r, c) will be set to the
-    other colour.
+    Check each (row, column) cell as specified by the rows and columns lists,
+    and using the mask ((r0, c0), (r1, c1)) to define the direction: for cell
+    (r, c), cells (r+r0, c+c0) and (r+r1, c+c1) will be considered contiguous;
+    if both are set to the same colour, and (r, c) is unset, (r, c) will be set
+    to the other colour.
 
-    The cells dictionary is directly updated. Returns True if any
-    changes have been made, False otherwise.
+    The cells dictionary is directly updated. Returns True if any changes have
+    been made, False otherwise.
     """
     changes = False
     for row in rows:
         for col in cols:
             this_cell = cells[(row, col)]
             if len(this_cell) == 1:
-                continue
+                continue  # skip if cell is already decided
             cell1 = cells[(row + mask[0][0], col + mask[0][1])]
             cell2 = cells[(row + mask[1][0], col + mask[1][1])]
             if cell1 == cell2 == 'r':
@@ -143,18 +142,19 @@ def eliminate_contiguous_line(cells, rows, cols, mask):
 
 def full_colour(problem):
     """
-    Update the problem by setting the cells that can be inferred
-    from the constraint that each row and column must have the
-    same number of cells of each colour.
+    Update the problem by setting the cells that can be inferred from the 
+    onstraint that each row and column must have the same number of cells of
+    each colour.
 
-    The cells dictionary is directly updated. Returns True if any
-    changes have been made, False otherwise.
+    The cells dictionary is directly updated. Returns True if any changes have
+    been made, False otherwise.
     """
     changes = False
     size = problem['size']
     cells_same_colour = size // 2
     cells = problem['variables']
-    # See if each row already has all the erquired cells of one colour
+
+    # See if each row already has all the required cells of one colour
     for row in range(size):
         reds = sum(1 for col in range(size) if cells[(row, col)] == 'r')
         blues = sum(1 for col in range(size) if cells[(row, col)] == 'b')
@@ -170,7 +170,8 @@ def full_colour(problem):
             if len(cells[(row, col)]) > 1:
                 cells[(row, col)] = new_colour
                 changes = True
-    # See if each column already has all the erquired cells of one colour
+
+    # See if each column already has all the required cells of one colour
     for col in range(size):
         reds = sum(1 for row in range(size) if cells[(row, col)] == 'r')
         blues = sum(1 for row in range(size) if cells[(row, col)] == 'b')
@@ -191,99 +192,94 @@ def full_colour(problem):
 
 def evaluate_state(problem, report=False):
     """
-    Check the state of a problem. This function does not return
-    anything, but modifies the 'state' field of the problem:
+    Check the state of a problem. This function does not return anything, but
+    modifies the 'state' field of the problem:
     - if a constraint is not met, it is set to 'infeasible'
-    - if all constraints are met and all cells are set, the
-      state is set to 'solved'
-    - otherwise it is left unmodified (usually this should be
-      'unsolved')
+    - if all constraints are met and all cells are set, the state is set to
+        'solved'
+    - otherwise it is left unmodified (usually this should be 'unsolved')
 
-    If report is set to True, the function will print the first
-    (if any) constraint violation it finds; since it returns
-    at that point, it is possible that additional violations
-    exist.
+    If report is set to True, the function will print the first (if any)
+    constraint violation it finds; since it returns at that point, it is
+    possible that additional violations exist.
     """
     # helper function to print violations if report is True
-    if report:
-        def conditional_print(*args):
-            """print() wrapper for logging violations"""
+    def conditional_print(*args):
+        """print() wrapper for logging violations"""
+        if report:
             print args
-    else:
-        def conditional_print(*args):
-            """print() wrapper to skip logging"""
-            pass
   
     cells = problem['variables']
     size = problem['size']
+    max_same_colour = size // 2
 
-    # row_or_col will be used first as column index, then as row index
-    # to avoid doing too loops
-    for row_or_col in range(size):
+    # idx will be used first as column index, then as row index
+    # to avoid doing two loops
+    for idx in range(size):
         # Check each column
-        col = [cells[(row, row_or_col)] for row in range(size)]
-        # infeasible if colum has more than size//2 reds
-        if sum(1 for cell in col if cell == 'r') > size // 2:
+        col = [cells[(row, idx)] for row in range(size)]
+        # infeasible if colum has more than max_same_colour reds
+        if sum(1 for cell in col if cell == 'r') > max_same_colour:
             problem['state'] = 'infeasible'
-            conditional_print('too many reds on column', row_or_col)
+            conditional_print('too many reds on column', idx)
             return
-        # infeasible if colum has more than n//2 blues
-        if sum(1 for cell in col if cell == 'b') > size // 2:
+        # infeasible if colum has more than max_same_colour blues
+        if sum(1 for cell in col if cell == 'b') > max_same_colour:
             problem['state'] = 'infeasible'
-            conditional_print('too many blues on column', row_or_col)
+            conditional_print('too many blues on column', idx)
             return
         # infeasible if three adjacent cells of the same colour
         for pos in range(size - 2):
             if col[pos] == col[pos+1] == col[pos+2] != 'rb':
                 problem['state'] = 'infeasible'
-                conditional_print('three in a row - column', row_or_col)
+                conditional_print('three in a row - column', idx)
                 return
         # check if the rest of the columns are duplicates of this one
-        for other_col in range(row_or_col + 1, size):
+        for other_col in range(idx + 1, size):
             col2 = [cells[(row, other_col)] for row in range(size)]
             same_reds = sum(1 for c1, c2 in zip(col, col2) if c1 == c2 == 'r')
             same_blues = sum(1 for c1, c2 in zip(col, col2) if c1 == c2 == 'b')
             # if one colour is fully duplicated, the column must be, too,
             # even if some cells are still unset (they will be set to the
             # other colour by constraint propagation)
-            if same_reds >= size // 2 or same_blues >= size // 2:
+            if same_reds >= max_same_colour or same_blues >= max_same_colour:
                 problem['state'] = 'infeasible'
-                conditional_print('duplicate columns', row_or_col, other_col)
+                conditional_print('duplicate columns', idx, other_col)
                 return
 
         # Check each row
-        row = [cells[(row_or_col, col)] for col in range(size)]
-        # infeasible if row has more than n//2 reds
-        if sum(1 for cell in row if cell == 'r') > size // 2:
+        row = [cells[(idx, col)] for col in range(size)]
+        # infeasible if row has more than max_same_colour reds
+        if sum(1 for cell in row if cell == 'r') > max_same_colour:
             problem['state'] = 'infeasible'
-            conditional_print('too many reds on row', row_or_col)
+            conditional_print('too many reds on row', idx)
             return
-        # infeasible if row has more than n//2 blues
-        if sum(1 for cell in row if cell == 'b') > size // 2:
+        # infeasible if row has more than max_same_colour blues
+        if sum(1 for cell in row if cell == 'b') > max_same_colour:
             problem['state'] = 'infeasible'
-            conditional_print('too many blues on row', row_or_col)
+            conditional_print('too many blues on row', idx)
             return
         for pos in range(size - 2):
             if row[pos] == row[pos+1] == row[pos+2] != 'rb':
                 problem['state'] = 'infeasible'
-                conditional_print('three in a row - row', row_or_col)
+                conditional_print('three in a row - row', idx)
                 return
         # check if the rest of the rows are duplicates of this one
-        for other_row in range(row_or_col + 1, size):
+        for other_row in range(idx + 1, size):
             row2 = [cells[(other_row, col)] for col in range(size)]
             same_reds = sum(1 for c1, c2 in zip(row, row2) if c1 == c2 == 'r')
             same_blues = sum(1 for c1, c2 in zip(row, row2) if c1 == c2 == 'b')
             # if one colour is fully duplicated, the column must be too
             # even if some cells are still unset (they will be set to the
             # other colour by constraint propagation)
-            if same_reds >= size // 2 or same_blues >= size // 2:
+            if same_reds >= max_same_colour or same_blues >= max_same_colour:
                 problem['state'] = 'infeasible'
-                conditional_print('duplicate rows', row_or_col, other_row)
+                conditional_print('duplicate rows', idx, other_row)
                 return
 
-    # check if solved
+    # check if solved <--> feasible and determined
     # at this point feasibility is guaranteed (see doc for solved())
-    if solved(problem):
+    if determined(problem):
         problem['state'] = 'solved'
 
 
@@ -291,9 +287,8 @@ def solve(problem):
     """
     Solve a 0hh1 problem
 
-    Returns the problem at the last stage in the solution
-    process. The problem 'state' field will be set to
-    'solved' or 'infeasible' as adequate.
+    Returns the problem at the last stage in the solution process. The problem
+    'state' field will be set to 'solved' or 'infeasible' as adequate.
     """
     constraints = [eliminate_contiguous, full_colour]
     solution = cp_solver.solve(problem, constraints, evaluate_state)
